d3dba03c7cdc494b3625f46e31936058072a305f
Tiago Katcipis <tiago.katcipis@neoway.com.br>
2015-04-07 15:20:20 -0300

    I want to delete ALL images

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -17,7 +17,7 @@ fi
 alias dockerkillall='docker kill $(docker ps -q)'
 ## Delete all stopped containers.
 alias dockercleanc='printf "\n>>> Deleting stopped containers\n\n" && docker rm $(docker ps -a -q)'
-## Delete all untagged images.
-alias dockercleani='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q -f dangling=true)'
+## Delete all images.
+alias dockercleani='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q)'
 ## Delete all stopped containers and untagged images.
 alias dockerclean='dockercleanc || true && dockercleani'
