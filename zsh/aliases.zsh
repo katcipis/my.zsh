98839390f19fa6a99a62a17198adc232796a3ad5
Tiago Katcipis <tiagokatcipis@gmail.com>
2018-12-26 01:40:49 -0200

    Fix vi alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,31 +12,16 @@ else
     alias history='fc -l 1'
 fi
 
-#XClip magic
-alias tox='xclip -selection clipboard'
-alias fromx='xclip -selection clipboard -o'
-
-#git
+# git
 alias gclean='git remote prune origin'
 
-#VPN
-alias vpnstart='sudo systemctl start openvpn-client@client.service'
-alias vpnstop='sudo systemctl stop openvpn-client@client.service'
-
-#Update arch
+# Update arch
 alias archupdate='sudo pacman -Sy archlinux-keyring && sudo pacman -Syyu'
 
-#Generate password
+# Generate password
 alias genpassword="openssl rand -base64 32"
 
-#Stats
+# Stats
 alias io="iostat -xmdz 1"
 
-# Neoway Network
-alias netstart="sudo systemctl start dhcpcd@enp1s0"
-
-# Go
-alias gohub="cd $HOME/workspace/go/src/github.com/NeowayLabs"
-alias golab="cd $HOME/workspace/go/src/gitlab.neoway.com.br"
-
-alias vi nvim
+alias vi="nvim"
