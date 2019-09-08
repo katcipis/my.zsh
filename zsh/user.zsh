ab26c22e5894a9b4a7a1fd68f0af04f7a7e9d0fa
Tiago Katcipis <tiago.katcipis@fromatob.com>
2019-09-08 12:30:35 +0200

    Remove ls alias

--- zsh/user.zsh
+++ zsh/user.zsh
@@ -17,10 +17,6 @@ setopt long_list_jobs
 # Setup the prompt with pretty colors
 setopt prompt_subst
 
-# ls colors
-export LSCOLORS="Gxfxcxdxbxegedabagacad"
-alias ls='ls --color=tty' || alias ls='ls -G'
-
 ## pager
 export PAGER="less"
 export LESS="-R"
