17507f6f5f2b05b8d44c679cdccccfccbd8b3fc6
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-03-17 18:28:07 +0100

    Add local bins

--- zsh/user.zsh
+++ zsh/user.zsh
@@ -24,4 +24,4 @@ export LESS="-R"
 export LC_CTYPE=$LANG
 export LC_ALL=en_US.UTF-8
 export EDITOR=vim
-export PATH=$LOCALDIR/go/bin:$HOME/go/bin:$PATH
+export PATH=$LOCALDIR/bin:$LOCALDIR/go/bin:$HOME/go/bin:$PATH
