a387de0ad991b0af01dd1b8392b45887cb2e7aab
Tiago Katcipis <tiagokatcipis@gmail.com>
2021-06-24 13:34:52 +0200

    feat: add alias to gpg encrypt with pub key

--- zsh/gpg.zsh
+++ zsh/gpg.zsh
@@ -1,3 +1,15 @@
 function gpg_export() {
     gpg --armor --export tiagokatcipis@gmail.com
 }
+
+function gpgc() {
+    local filepath="${1}"
+
+    if [[ -z "${filepath}" ]] then
+        echo "file to be encrypted is obligatory"
+        printf "usage: %s <file path>\n" ${0}
+        return
+    fi
+
+    gpg --encrypt --recipient tiagokatcipis@gmail.com "${filepath}"
+}
