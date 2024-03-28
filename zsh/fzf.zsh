82645f72148ce8a7bac1a6113abe5993cee2022a
Tiago Katcipis <tiagokatcipis@gmail.com>
2024-03-28 11:48:13 +0100

    feat: update fzf install

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,10 +1,1 @@
-fzfkeys=/usr/share/fzf/key-bindings.zsh
-fzfcomp=/usr/share/fzf/completion.zs
-
-if [ -f $fzfkeys ]; then
-    source $fzfkeys
-fi
-
-if [ -f $fzfcomp ]; then
-    source $fzfcomp
-fi
+eval "$(fzf --zsh)"
